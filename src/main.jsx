bf4f2116ef414137fb108c43cc824ad49482051f
dgenutis <dainiusgenutis@yahoo.com>
2024-10-01 13:21:36 +0300

    navbar+header

---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+/**
+ * @copyright 2024 Dainius Genutis
+ * @license Apache-2.0
+ */
+
+/**
+ *  Node modules
+ */
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+
+
+/**
+ *  Components
+ */
+import App from "./App.jsx";
+
+
+/**
+ * CSS link
+ */
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
