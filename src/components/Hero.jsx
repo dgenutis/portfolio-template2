a84eae817e955bc91238eec05ec36a78165d4520
dgenutis <dainiusgenutis@yahoo.com>
2024-10-02 20:39:46 +0300

    hero done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,6 +5,13 @@
 
 
 
+
+/**
+ * Components
+ */
+
+import { ButtonPrimary, ButtonOutline } from "./Button";
+
 const Hero = () => {
   return (
     <section 
@@ -32,21 +39,24 @@ const Hero = () => {
               Available for work
             </div>
           </div>
-          <h2 className="headline-1">
+          <h2 className="headline-1 max-w-[15ch] sm:max-w-[20ch] lg:max-w-[15ch] mt-5 mb-8 lg:mb-10">
             Building Scalable Modern Websites for the Future
             </h2>
-          <div className="">
-              ButtonPrimary
+          <div className="flex items-center gap-3">
+              <ButtonPrimary 
+              label="Download CV"
+              icon="download"
+              />
 
-              ButtonOutline
+              <ButtonOutline />
 
           </div>
         </div>
 
-        <div className="">
-          <figure className="">
+        <div className="hidden lg:block">
+          <figure className="w-full max-w-[480] ml-auto bg-gradient-to-t from-sky-400 via-25% via-sky-400/40 to-65% rounded-[60px] overflow-hidden">
             <img 
-            src="/2.jpg"
+            src="/3.png"
             width={656}
             height={800} 
             alt="Dainius Genutis"
